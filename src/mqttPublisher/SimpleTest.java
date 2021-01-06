911aad745b568bab343c132d2ca0f4c8f58c0700
John <Johnschroedercs@gmail.com>
2021-01-06 11:14:28 -0700

    Restructuring.

---
+++ src/mqttPublisher/SimpleTest.java
@@ -0,0 +1,20 @@
+package mqttPublisher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class SimpleTest {
+
+	@Test
+	void passTest() {
+		assertTrue(true);
+	}
+
+	@Test
+	void failTest() {
+		assertTrue(false);
+	}
+
+}
